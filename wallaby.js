96b41f49bfcb8fd9c07dbf3cda3d0864881ef4c5
Artem Govorov <artem.govorov@gmail.com>
2018-07-01 11:59:04 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,16 +1,24 @@
 // tslint:disable
 module.exports = function wallabyConfig(wallaby) {
+  const compiler = wallaby.compilers.babel({
+    plugins: [
+      ['@babel/proposal-decorators', {legacy: true}],
+      '@babel/proposal-class-properties'
+    ],
+    presets: ['@zeit/next-typescript/babel']
+  });
   return {
     files: [
       '@hd/**/*.ts',
-      // 'components/**/**.vue',
+      'components/**/**.vue',
       'tsconfig.json'
     ],
     tests: ['test/**/*.spec.ts'],
     compilers: {
       '**/*.ts?(x)': wallaby.compilers.typeScript({
         module: 'commonjs'
-      })
+      }),
+      '**/*.vue': require('wallaby-vue-compiler')(compiler),
     },
     preprocessors: {
       '**/*.vue': file => require('vue-jest').process(file.content, file.path),
